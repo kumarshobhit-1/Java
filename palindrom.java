4291a02a8c34196cb6b4e2ba4282e5d137c26f09
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-02-06 22:15:41 +0530

    Refactor palindrome class to improve code structure and readability

--- palindrom.java
+++ palindrom.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 public class palindrom {
-    
+    public static boolean isPalindrome(int number) {
+        int palindrome = number; // copied number into variable
+        int reverse = 0;
+        while (palindrome != 0) {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+        if (number == reverse) {
+            return true;
+        }
+            return false;
+    }
     public static void main(String args[]) {
         System.out.print("Please Enter a number : ");
         Scanner sc = new Scanner(System.in);
@@ -12,18 +24,5 @@ public class palindrom {
         }
         sc.close();
     }
-    public static boolean isPalindrome(int number) {
-        int palindrome = number; // copied number into variable
-        int reverse = 0;
-        while (palindrome != 0) {
-            int remainder = palindrome % 10;
-            reverse = reverse * 10 + remainder;
-            palindrome = palindrome / 10;
-        }
-        if (number == reverse) {
-            return true;
-            }
-            return false;
-        }
-    }
+}
 

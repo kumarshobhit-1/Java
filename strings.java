10318b20234ce9964f84c6133835ffaba3f4d0bf
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-02-06 22:10:17 +0530

    Add strings class to print letters of a full name

---
+++ strings.java
@@ -0,0 +1,16 @@
+public class strings {
+    public static void printLetters(String str){
+        for(int i=0; i<str.length(); i++){
+            System.out.print(str.charAt(i) + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        String firstName = "shobhit";
+        String lastName = "Kumar";
+        String fullName = firstName + " " + lastName;
+
+        printLetters(fullName);
+    }
+}
+ 

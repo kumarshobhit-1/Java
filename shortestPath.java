1151241d4d35b32e0ab38d8613a4606c9b72cd7b
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-02-08 18:53:55 +0530

    Add shortestPath class to calculate the shortest distance based on directional input

---
+++ shortestPath.java
@@ -0,0 +1,20 @@
+public class shortestPath {
+    public static float getShortestPath(String path){
+        int x=0, y=0;
+        for(int i=0; i<path.length(); i++){
+            char dir = path.charAt(i);
+
+            if (dir == 'S')  y-- ; // south
+            else if (dir == 'N') y++; //north
+            else if(dir == 'W') x--; //west
+            else x++; //east
+        }
+        int X2 = x*x;
+        int Y2 = y*y;
+        return (float)Math.sqrt(X2 + Y2);
+    }
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println(getShortestPath(path));
+    }
+}

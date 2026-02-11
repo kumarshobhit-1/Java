412241c018f71f7c9cd06e2d27d4224f745f3dc5
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-02-11 19:38:28 +0530

    add bitwise operator

---
+++ bitWise.java
@@ -0,0 +1,16 @@
+public class bitWise {
+    public static void main(String[] args) {
+        // Binary AND (&) operator
+        System.out.println((5 & 6));
+        // Binary OR (|) operator
+        System.out.println((5 | 6));
+        // Binary XOR (^) operator
+        System.out.println((5 ^ 6));
+        // Binary one's compliment (~) operator
+        System.out.println((~5));
+        // Binary Left shift (<<) operator
+        System.out.println((5 << 2));
+        // Binary roght shift (>>) operator
+        System.out.println((6 >> 1));
+    }
+}

5e6603cac47749f10af43ec53d29596bae3ce2bc
kumarshobhit-1 <shobhitkumar1437@gmail.com>
2026-02-19 01:39:23 +0530

    refactor: update Mystudent constructor usage in main method

--- cons.java
+++ cons.java
@@ -1,8 +1,9 @@
 public class cons {
     public static void main(String[] args) {
-        // Mystudent s1 = new Mystudent("shobhit");
         Mystudent s1 = new Mystudent();
-        // System.out.println(s1.name);
+        Mystudent s2 = new Mystudent("shobhit", 12);
+        System.out.println(s2.name);
+        System.out.println(s2.roll);
     }
 }
 
@@ -10,12 +11,16 @@ class Mystudent {
     String name;
     int roll;
 
-    // Mystudent(String name) {
-    //     this.name = name;
-    // }
-    public Mystudent() {
+    Mystudent() {
         System.out.println("Constructor is called...");
     }
+
+    Mystudent(String name, int roll) {
+        this.name = name;
+        this.roll = roll;
+    }
+
+    
 
     
 }

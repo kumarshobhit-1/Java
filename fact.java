5a82340d8264471bc76600363c71bc2c5fdbcab4
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-03-13 19:51:42 +0530

    add: implement factorial calculation using recursion

---
+++ fact.java
@@ -0,0 +1,16 @@
+public class fact {
+    // find factorial number using recursion
+    public static int fact1(int n){
+        if (n == 0) {
+            return 1;
+        }
+        int fnm = fact1(n);
+        int fn = n * fact1(n-1);
+        return fn;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.err.println(fact1(n));
+    }
+}

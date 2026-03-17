68cd1a4a224f12b04764da8a13e2dce0e86017ee
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-03-17 23:11:07 +0530

    add: implement method to find the last occurrence of a key in an array using recursion

---
+++ lastOccurence.java
@@ -0,0 +1,16 @@
+public class lastOccurence {
+    public static int foundLastOccurence(int arr[], int key, int i){
+        if (i == arr.length) {
+            return -1;
+        }
+        int isFound = foundLastOccurence(arr, key, i+1);
+        if (isFound == -1 && arr[i] == key) {
+            return i;
+        }
+        return isFound;
+    }
+    public static void main(String[] args) {
+        int arr[] = {8,3,5,6,9,5,10,2,3};
+        System.out.println(foundLastOccurence(arr, 5, 0));
+    }
+}

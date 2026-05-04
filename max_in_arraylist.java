7ee3aea71572daf3ba26d7d3cb112360254eef8d
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-05-04 14:48:56 +0530

    fix: correct indentation in max_in_arraylist main method

--- max_in_arraylist.java
+++ max_in_arraylist.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class max_in_arraylist {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
 
         list.add(2);

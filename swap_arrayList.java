dc741b5b597093a5b1f23e2b1e2cf586f4841c28
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-06-23 21:38:23 +0530

    swap_arrayList

--- swap_arrayList.java
+++ swap_arrayList.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 
 public class swap_arrayList {
+    public static void swap(ArrayList<Integer> list, int idx1, int idx2){
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
+    }
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
 
@@ -10,6 +15,10 @@ public class swap_arrayList {
         list.add(3);
         list.add(6);
         
+        int idx1=1, idx2=3;
+        System.out.println(list);
+        swap(list, idx1, idx2);
+        System.out.println(list);
         
     }
 }

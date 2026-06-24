2c6aaf9e833203b0e575291816afc8d66d78869b
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-06-24 22:07:37 +0530

    add pairSum_one

---
+++ pairSum_one.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class pairSum_one {
+    // Brute Force O(n^2)
+    public static boolean pairSum1(ArrayList<Integer> list, int target){
+        for(int i=0; i<list.size(); i++){
+            for(int j=i+1; j<list.size(); j++){
+                if (list.get(i) + list.get(j) == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Two pointer 
+    public
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        int target = 5;
+        System.out.println(pairSum1(list, target));
+    }
+}

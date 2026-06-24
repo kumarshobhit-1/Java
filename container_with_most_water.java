1728b4c0d3774df3a1a5d29b6ff04360c257d75e
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-06-24 19:31:31 +0530

    add 2 pointer approach in container with most water

--- container_with_most_water.java
+++ container_with_most_water.java
@@ -1,15 +1,38 @@
 import java.util.ArrayList;
 
 public class container_with_most_water {
+    //Brute Force - O(n^2)
     public static int storeWater(ArrayList<Integer> height){
         int maxWater = 0;
-        //Brute Force - O(n^2)
         for(int i=0; i<height.size(); i++){
             for(int j=i+1; j<height.size(); j++){
                 int ht = Math.min(height.get(i), height.get(j));
                 int width = j-1;
                 int currWater = ht * width;
                 maxWater = Math.max(maxWater, currWater);
+            }
+        }
+        return maxWater;
+    }
+
+    // Two Pointer - O(n)
+    public static int storedWater(ArrayList<Integer> height){
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.size()-1;
+
+        while (lp < rp) {
+            // calculate water area
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int width = rp-lp;
+            int currWater = ht * width;
+            maxWater = Math.max(maxWater, currWater);
+
+            //updated pointer
+            if (height.get(lp) < height.get(rp)) {
+                lp++;
+            } else {
+                rp--;
             }
         }
         return maxWater;
@@ -28,5 +51,6 @@ public class container_with_most_water {
         height.add(7);
 
         System.out.println(storeWater(height));
+        System.out.println(storedWater(height));
     }
 }

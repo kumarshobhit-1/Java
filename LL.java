38b5e32cfd86a738788a278c90cd06838ed0f533
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-06-27 22:28:46 +0530

    add new node at the head(addFirst) in linked list

--- LL.java
+++ LL.java
@@ -13,9 +13,30 @@ public static LinkedList{
 
     public static Node head;
     public static Node tail;
+
+    public void addFirst(int data){
+        
+        //step1 = create new node
+        Node newNode = new Node(data);
+
+        if (head == null) { //if node is NULL
+            head = tail = newNode;
+            return;
+        }
+
+        // if node one or more
+        //step2 - newNode next = head
+        newNode.next = head; //link
+
+        //step3 - head = newNode
+        head = newNode;
+
+    }
 }
 public class LL {
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
+
+        
     }
 }

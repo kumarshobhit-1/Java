cfbf9fe3dfd622779e5828994f73c740d456678b
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-06-27 22:34:44 +0530

    add node at the end(tail) in the linked list

--- LL.java
+++ LL.java
@@ -32,11 +32,31 @@ public static LinkedList{
         head = newNode;
 
     }
+
+    public void addLast(int data){
+        //step1 = create new node
+        Node newNode = new Node(data);
+
+        if (head == null) { //if node is NULL
+            head = tail = newNode;
+            return;
+        }
+
+        // if node one or more
+        //step2 - tail.next = newNode
+        tail.next = newNode; //link
+
+        //step3 - tail = newNode
+        tail = newNode;
+    }
 }
 public class LL {
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
 
-        
     }
 }

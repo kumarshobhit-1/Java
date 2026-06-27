30c7d609a8667de5939b36723d4b457a0ebe31b9
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-06-27 20:25:29 +0530

    add head and tail class in LL

--- LL.java
+++ LL.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 public static LinkedList{
     public static class Node{
         int data;
@@ -8,9 +10,12 @@ public static LinkedList{
         this.data = data;
         this.next = next;
     }
+
+    public static Node head;
+    public static Node tail;
 }
 public class LL {
     public static void main(String[] args) {
-        
+        LinkedList ll = new LinkedList();
     }
 }

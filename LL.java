7b032077c5213e182a385b11e0ab54f147f513cf
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-06-27 20:15:17 +0530

    create linked list

---
+++ LL.java
@@ -0,0 +1,16 @@
+public static LinkedList{
+    public static class Node{
+        int data;
+        Node next;
+    }
+
+    public Node(int data){
+        this.data = data;
+        this.next = next;
+    }
+}
+public class LL {
+    public static void main(String[] args) {
+        
+    }
+}

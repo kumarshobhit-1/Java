7f2afa7d28d651eb759198d241f192a090d65af1
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-06-28 00:01:16 +0530

    method add middle node at the linked list

--- LinkedList.java
+++ LinkedList.java
@@ -60,6 +60,25 @@ public class LinkedList{
         }
         System.out.println("null");
     }
+
+    public void addMiddle(int idx, int data){
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while (i < idx-1) {
+            temp = temp.next;
+            i++;
+        }
+
+        // i = idx-1; temp -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.print();
@@ -70,6 +89,8 @@ public class LinkedList{
         ll.addLast(3);
         ll.print();
         ll.addLast(4);
+        ll.print();
+        ll.addMiddle(2, 9);
         ll.print();
     }
 

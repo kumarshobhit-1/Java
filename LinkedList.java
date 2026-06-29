18d95af411056adaa63e889a8c9137c051cd82f5
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-06-29 22:45:42 +0530

    write code for zig zag problem

--- LinkedList.java
+++ LinkedList.java
@@ -323,6 +323,48 @@ public class LinkedList{
         return merge(newLeft, newRight);
     }
 
+
+    // Zig - Zag finction code
+    public void zigZag(){
+        //find mid
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        // reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        //alternate merge - zig-zag merge
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            right = nextR;
+            left = nextL;
+        }
+
+    }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         // ll.print();
@@ -363,18 +405,28 @@ public class LinkedList{
 
         // System.out.println(ll.checkPalindrome());
 
-        ll.addFirst(1);
-        ll.addFirst(2);
-        ll.addFirst(3);
-        ll.addFirst(4);
-        ll.addFirst(5);
+        // ll.addFirst(1);
+        // ll.addFirst(2);
+        // ll.addFirst(3);
+        // ll.addFirst(4);
+        // ll.addFirst(5);
 
-        System.out.println("Before Sorting:");
+        // System.out.println("Before Sorting:");
+        // ll.print();
+
+        // ll.head = ll.mergeSort(ll.head); //O(nlogn)
+
+        // System.out.println("After Sorting:");
+        // ll.print();
+
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
         ll.print();
-
-        ll.head = ll.mergeSort(ll.head); //O(nlogn)
-
-        System.out.println("After Sorting:");
+        ll.zigZag();
         ll.print();
     }
 

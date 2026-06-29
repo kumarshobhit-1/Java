f3152d27ac97359df6b6421536e9ecfd163d7b92
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-06-29 22:59:21 +0530

    write code for addFirst list doubley linked list

---
+++ DoubleLL.java
@@ -0,0 +1,63 @@
+import org.w3c.dom.Node;
+
+public class DoubleLL {
+    public class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    //add
+    //addFirst
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+
+    //addLast home work
+
+    //remove
+    //removeFirst
+
+
+    //removeLast
+    //homework
+
+    // print function
+    public void print(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data+" <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        DoubleLL dll = new DoubleLL();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+
+        dll.print();
+        System.out.println(dll.size);
+    }
+}

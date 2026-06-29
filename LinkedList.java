b4bdbb40173a0dc39b23bad22a7dcc5e50806623
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-06-29 19:51:50 +0530

    write code for mreger sort by Linked list

--- LinkedList.java
+++ LinkedList.java
@@ -264,6 +264,65 @@ public class LinkedList{
     }
 
 
+    private Node getMid(Node head){
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; //mid Node
+    }
+
+    private Node merge(Node head1, Node head2){
+        Node mergeLL = new Node(-1);
+        Node temp = mergeLL;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergeLL.next;
+    }
+
+    public Node mergeSort(Node head){
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        //find mid
+        Node mid = getMid(head);
+
+        //left & right MS
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+
+        //merge
+        return merge(newLeft, newRight);
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         // ll.print();
@@ -296,13 +355,27 @@ public class LinkedList{
         // ll.deleteNthfromEnd(3);
         // ll.print();
 
-        ll.addLast(1);
-        ll.addLast(2);
-        ll.addLast(2);
-        ll.addLast(1);
+        // ll.addLast(1);
+        // ll.addLast(2);
+        // ll.addLast(2);
+        // ll.addLast(1);
+        // ll.print();
+
+        // System.out.println(ll.checkPalindrome());
+
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+
+        System.out.println("Before Sorting:");
         ll.print();
 
-        System.out.println(ll.checkPalindrome());
+        ll.head = ll.mergeSort(ll.head);
+
+        System.out.println("After Sorting:");
+        ll.print();
     }
 
 

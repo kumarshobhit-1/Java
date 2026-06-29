65441fc557f4caad70591beca482892d29b48aeb
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-06-29 23:22:05 +0530

    write code for remove last element

--- DoubleLL.java
+++ DoubleLL.java
@@ -74,7 +74,27 @@ public class DoubleLL {
 
     //removeLast
     //homework
-    
+        public int removeLast(){
+        if (size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        //prev : i = size-2
+        Node prev = head;
+        for(int i=0; i<size-2; i++){
+            prev = prev.next;
+        }
+
+        int val = prev.next.data;
+        prev.next = null;
+        size--;
+        return val;
+    }
 
     //Reverse the doubley Linked list
     public void reverseDLL(){
@@ -111,6 +131,9 @@ public class DoubleLL {
         dll.addLast(4);
         dll.print();
         dll.addLast(5);
+        dll.print();
+        System.out.println(dll.size);
+        dll.removeLast();
         dll.print();
         System.out.println(dll.size);
         

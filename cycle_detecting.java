8414bad2a12bb4c30a8200fd74de336c027ad0a4
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-06-29 18:26:46 +0530

    write function for remove cycle in Linked list

--- cycle_detecting.java
+++ cycle_detecting.java
@@ -11,6 +11,7 @@ public class cycle_detecting {
 
     public static Node head;
 
+    // cycle detecting
     public static boolean isCycle(){ //floyd's cycle detecting algorithm
         Node slow = head;
         Node fast = head;
@@ -25,13 +26,50 @@ public class cycle_detecting {
 
         return false; // cycle doesn't exist
     }
+
+    // remove cycle (agar head se connect nhi karna hai)
+    public static void removeCycle(){
+        // detect cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                cycle = true;
+                break;
+            }
+        }
+
+        if (cycle == false) {
+            return;
+        }
+
+        // find meeting point
+        slow = head;
+        Node prev = null; // last node
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        //remove cycle -> last.next = null
+        prev.next = null;
+        
+    }
+
     public static void main(String[] args) {
         head = new Node(1);
-        head.next = new Node(2);
+        Node temp = new Node(2);
+        head.next = temp;
         head.next.next = new Node(3);
-        head.next.next.next = head;
+        head.next.next.next = temp;
 
-        // 1->2->3->1
+        // 1->2->3->2
+        System.out.println(isCycle());
+        removeCycle();
         System.out.println(isCycle());
     }
 }

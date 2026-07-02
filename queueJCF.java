81b6eadc0a4640db7ea5019d1354bfb10be41797
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-07-02 21:03:15 +0530

    queue using java collection framwork by Linked list

---
+++ queueJCF.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class queueJCF { //JCF => java collection framework
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}

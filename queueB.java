0df964fb548c0f6daf67c65bd66b6a26b8d234b6
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-07-02 19:16:12 +0530

    update the code and improve complexiti fron O(n) to O(1)

--- queueB.java
+++ queueB.java
@@ -3,25 +3,36 @@ public class queueB {
         static int arr[];
         static int size;
         static int rear;
+        static int front;
 
         Queue(int n){
             arr = new int[n];
             size = n;
             rear = -1;
+            front = -1;
         }
 
         public static boolean isEmpty(){
-            return rear == -1;
+            return rear == -1 && front == -1;
+        }
+
+
+        public static boolean isFull(){
+            return (rear + 1)% size == front;
         }
 
         // add function
         public static void add(int data){
-            if (rear == size-1) {
+            if (isFull()) {
                 System.out.println("queue is full");
                 return;
             }
 
-            rear = rear +1;
+            // add 1st element
+            if (front == -1) {
+                front = 0;
+            }
+            rear = (rear +1) % size;
             arr[rear] = data;
         }
 
@@ -33,12 +44,15 @@ public class queueB {
                 return -1;
             }
 
-            int front = arr[0];
-            for(int i=0; i<rear; i++){
-                arr[i] = arr[i+1];
+            int result = arr[front];
+
+            //last el delete
+            if (rear == front) {
+                rear = front = -1;
+            } else {
+                front = (front + 1)% size;
             }
-            rear = rear - 1;
-            return front;
+            return result;
         }
 
 
@@ -49,7 +63,7 @@ public class queueB {
                 return -1;
             }
 
-            return arr[0];
+            return arr[front];
         }
     }
     public static void main(String[] args) {
@@ -57,6 +71,10 @@ public class queueB {
         q.add(1);
         q.add(2);
         q.add(3);
+        System.out.println(q.remove());
+        q.add(4);
+        System.out.println(q.remove());
+        q.add(5);
         // 1 2 3
         while (!q.isEmpty()) {
             System.out.println(q.peek());

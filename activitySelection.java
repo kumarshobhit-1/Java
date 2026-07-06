bc66fbf375bb77969ffe3d437cb4af9694b41a50
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-07-06 22:24:43 +0530

    write comparator for the sort java objects

--- activitySelection.java
+++ activitySelection.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class activitySelection {
     public static void main(String[] args) { //O(n)
@@ -14,6 +16,8 @@ public class activitySelection {
         }
 
         //lambda function(shortForm) -> In Java, Comparator is an interface for sorting java OBjects.
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
 
         // end time basis sorted
         int maxAct = 0;
@@ -21,14 +25,14 @@ public class activitySelection {
 
         //1st activity
         maxAct = 1;
-        ans.add(0);
-        int lastEnd = end[0];
+        ans.add(activities[0][0]);
+        int lastEnd = activities[0][2];
         for(int i=0; i<end.length; i++){
-            if (start[i] >= lastEnd) {
+            if (activities[i][1] >= lastEnd) {
                 //activity select
                 maxAct++;
-                ans.add(i);
-                lastEnd = end[i];
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
             }
         }
 

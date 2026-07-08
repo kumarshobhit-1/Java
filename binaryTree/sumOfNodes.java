5ad36dc0e9df8d1252ff931290f648be5f643c9b
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-07-08 20:08:04 +0530

    Add sumOfNodes class with node summation functionality and main method execution

---
+++ binaryTree/sumOfNodes.java
@@ -0,0 +1,43 @@
+package binaryTree;
+
+public class sumOfNodes {
+    public static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    public static int sumOfNode(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftSum = sumOfNode(root.left);
+        int rightSum = sumOfNode(root.right);
+        return leftSum + rightSum + root.data;
+    }
+
+    public static void main(String[] args) {
+        /*
+                    1
+                   / \
+                  2   3
+                 / \  /\
+                4   56  7
+        */
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println("Sum of the node is: " + sumOfNode(root));
+    }
+}

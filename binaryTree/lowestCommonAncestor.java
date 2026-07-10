73b124058ef9a4bc66f11b343c780e1233e25529
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-07-10 19:42:21 +0530

    Add lowest common ancestor functionality for binary tree with main method execution

---
+++ binaryTree/lowestCommonAncestor.java
@@ -0,0 +1,78 @@
+package binaryTree;
+import java.util.ArrayList;
+
+public class lowestCommonAncestor {
+    public static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if (root == null) {
+            return false;
+        }
+        path.add(root);
+        if (root.data == n) {
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if (foundLeft || foundRight) {
+            return true;
+        }
+        path.remove(path.size() - 1);
+        return false;
+    }
+
+    public static Node findLowestCommonAncestor(Node root, int n1, int n2) { // O(n)
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        //last common ancestor
+        int i = 0;
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+
+        //last equal node -> i-1th node
+        Node lca =  path1.get(i - 1);
+        return lca;
+    }
+
+    public static void main(String[] args) {
+        /* main tree
+                    1
+                   / \
+                  2   3
+                 / \  /\
+                4   56  7
+        */
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int n1 = 4; 
+        int n2 = 7; 
+
+        Node lca = findLowestCommonAncestor(root, n1, n2);
+        System.out.println("Lowest Common Ancestor of " + n1 + " and " + n2 + " is: " + lca.data);
+    }
+}

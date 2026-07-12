3bf8d4218eae8f82b11722f0af8ea5647cab91be
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-07-12 11:37:06 +0530

    print in range between two nodes

--- binarySearchTree/deleteBST.java
+++ binarySearchTree/deleteBST.java
@@ -70,6 +70,22 @@ public class deleteBST {
         }
         return root;
     }
+
+    //Print In range between 2 nodes
+    public static void printInRange(Node root, int k1, int k2){
+        if (root == null) {
+            return;
+        }
+        if (root.data >= k1 && root.data <= k2) {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data+" ");
+            printInRange(root.right, k1, k2);
+        } else if (root.data < k1) {
+            printInRange(root.left, k1, k2);
+        } else {
+            printInRange(root.right, k1, k2);
+        }
+    }
     public static void main(String[] args) {
         int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root = null;
@@ -81,9 +97,12 @@ public class deleteBST {
         inorder(root);
         System.out.println();
 
-        root = delete(root, 5);
-        System.out.println();
+        // root = delete(root, 5);
+        // System.out.println();
 
-        inorder(root);
+        // inorder(root);
+
+        int k1 = 5, k2 = 12;
+        printInRange(root, k1, k2);
     }
 }

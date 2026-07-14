3d39f7ce5dab4b32810d6a0c4c8f167bbdf8a6aa
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-07-14 21:53:56 +0530

    feat: add PriorityQueue implementation example using custom Student objects

--- heaps/pqInJCF.java
+++ heaps/pqInJCF.java
@@ -7,17 +7,31 @@ import java.util.PriorityQueue;
  * pqInJCF
  */
 public class pqInJCF {
+    static class Student implements Comparable<Student> { // overriding
+        String name;
+        int rank;
+
+        public Student(String name, int rank){
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student s2){
+            return this.rank - s2.rank;
+        }
+    }
 
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Student> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
-        pq.add(3); //O(log n)
-        pq.add(4);
-        pq.add(1);
-        pq.add(7);
-        
+        pq.add(new Student("A", 4));
+        pq.add(new Student("B", 5));
+        pq.add(new Student("C", 2));
+        pq.add(new Student("D", 12));
+
         while (!pq.isEmpty()) {
-            System.out.println(pq.peek()); //O(1)
+            System.out.println(pq.peek().name + " -> " + pq.peek().rank); //O(1)
             pq.remove(); //O(log n)
         }
     }

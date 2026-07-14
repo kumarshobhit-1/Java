959a3a3a3d8712bfdf825e16412194f18b2cbb3e
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-07-14 21:36:11 +0530

    feat: implement priority queue demonstration using Java Collections Framework

---
+++ heaps/pqInJCF.java
@@ -0,0 +1,24 @@
+package heaps;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * pqInJCF
+ */
+public class pqInJCF {
+
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(3); //O(log n)
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+        
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek()); //O(1)
+            pq.remove(); //O(log n)
+        }
+    }
+}

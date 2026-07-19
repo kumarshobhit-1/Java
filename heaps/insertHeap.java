d36446dabe07e5a6c94dc53ed57ee454b14ebb3c
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-07-19 14:08:52 +0530

    feat: implement min-heap data structure with add, remove, and heapify functionality

--- heaps/insertHeap.java
+++ heaps/insertHeap.java
@@ -18,6 +18,9 @@ public class insertHeap {
                 int temp = arr.get(x);
                 arr.set(x, arr.get(par));
                 arr.set(par, temp);
+
+                x = par;
+                par = (x-1)/2;
             }
         }
 
@@ -28,7 +31,25 @@ public class insertHeap {
 
         private void heapify(int i){
             int left = 2*i+1;
-            int right = 2*
+            int right = 2*i*2;
+            int minIdx = i;
+
+            if(left  < arr.size() && arr.get(minIdx) > arr.get(left)){
+                minIdx = left;
+            }
+
+            if(right  < arr.size() && arr.get(minIdx) > arr.get(right)){
+                minIdx = right;
+            }
+
+            if (minIdx != i) {
+                // swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                heapify(minIdx);
+            }
         }
 
         public int remove(){
@@ -36,7 +57,7 @@ public class insertHeap {
 
             // step 1 - swap first & last
             int temp = arr.get(0);
-            arr.set(0, arr.size()-1);
+            arr.set(0, arr.get(arr.size()-1));
             arr.set(arr.size()-1, temp);
 
 
@@ -44,10 +65,24 @@ public class insertHeap {
             arr.remove(arr.size()-1);
 
             //step 3 - heapify
+            heapify(0);
+            return data;
 
+        }
+        public boolean isEmpty(){
+            return arr.size() == 0;
         }
     }
     public static void main(String[] args) {
+        Heap h = new Heap();
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
         
+        while (!h.isEmpty()) {
+            System.out.print(h.peek());
+            h.remove();
+        }
     }
 }
